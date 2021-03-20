48b2cc9ed2f454c759df152f175f02cebb4bbc42
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-03-20 12:58:23 +0100

    until nr 3

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-let cars = ['volvo', 'saab', 'bmw', 'tesla'];  console.log(cars[0]);   console.log(cars[1]);  console.log(cars[2]);console.log(cars[3]);Kod 2let cars = ['volvo', 'saab', 'bmw', 'tesla'];  let fruits = ['apple', 'banana', 'orange'];  console.log(cars[0]);   console.log(cars[1]);  console.log(cars[2]);  console.log(cars[3]);console.log(fruits[0]);  console.log(fruits[1]);  console.log(fruits[2]); Kod 3Den här koden må följa DRY-principen, men namn konventionen är inte den bästa. Kan du förbättraden?let x = ['rose', 'tulip', 'lily', 'orchid'];  functionasdasd(banana) {  for (let i = 0; i < banana.length; i++) {        console.log(banana[i]);      }    }    asdasd(x);
+//1
+// let cars = ['volvo', 'saab', 'bmw', 'tesla'];  
+// console.log(cars);   
+// for (let car of cars){
+//     console.log(car)
+// }
+
+//2
+// let cars = ['volvo', 'saab', 'bmw', 'tesla'];  
+// let fruits = ['apple', 'banana', 'orange'];  
+// function print(toprint) {
+//     for(let obj of toprint)
+//     console.log(obj);
+// }
+
+//  print(cars);
+//  print(fruits);
+
+
+// console.log(cars[0]);   
+// console.log(cars[1]);  
+// console.log(cars[2]);  
+// console.log(cars[3]);
+// console.log(fruits[0]);  
+// console.log(fruits[1]);  
+// console.log(fruits[2]); 
+
+//3
+let flowers = ['rose', 'tulip', 'lily', 'orchid'];  
+function print(flowers) {  
+    for (let flower = 0; flower < flowers.length; flower++) {        
+        console.log(flowers[flower]);      
+    }    
+}    
+print(flowers);
